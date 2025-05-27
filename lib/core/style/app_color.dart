d5fdfbfcf73398aa24a4ef1dd9f9872ae5f2fd76
RenanVolpe <desenvolvimento19@dixiponto.com.br>
2025-05-27 15:28:55 -0300

    feat: add colors and text styles

--- lib/core/style/app_color.dart
+++ lib/core/style/app_color.dart
@@ -1,18 +1,56 @@
 import 'package:flutter/material.dart';
 
-class AppColor {
-  static const Color kPrimary = Color(0xFF1E4DC5);
-  static const Color kPrimaryLight = Color(0xFF61A4E6);
-  static const Color kBlackText = Color(0xFF212121);
-  static const Color kBlack = Color(0xFF000000);
-  static const Color kWhite = Color(0xFFFFFFFF);
-  static const Color kBackgroundLight = Color(0xFFF5F5F5);
-  static const Color kBackground = Color(0xFFE8E8E8);
-    static const Color grey1 = Color(0xFF333333);
-  static const Color kgrey2 = Color(0xFF666666);
-  static const Color kgrey3 = Color(0xFFF1F0F0);
+class AppColors {
+  static const Color primary = Color(0xFF49A8DB);
+  static const Color primaryLight = Color(0xFFC8E5F4);
+  static const Color primaryDark = Color(0xFF36799D);
 
-  static const Color kPrimaryDark = Color(0xFF15305B);
+  static const Color listViewBackground = Color(0xFFF9F9F9);
+  static const Color menuInput = Color(0xFFF0F9FF);
+  static const Color whiteLight = Color(0xFFF9FAFA);
+  static const Color text = Color(0xFF7A7A7A);
+  static const Color listBorderHover = Color(0x4649A8DB);
 
+  static const Color redActive = Color(0xFFFADFFF);
+  static const Color redBorder = Color(0xffDBAAE4);
+  static const Color redLight1 = Color(0xffFFCCCC);
+  static const Color redBox = Color(0xFFFFC2C2);
+  static const Color redLight2 = Color(0xffF99D9D);
+  static const Color redText = Color(0xffFF0000);
+  static const Color redDark = Color(0xffD30505);
 
+  static const Color greenBox = Color(0xFFD9EAD3);
+  static const Color greenFill = Color(0xFFE1FFD6);
+  static const Color greenLight1 = Color(0xFFC7E4BD);
+  static const Color greenActive = Color(0xFFC7E4BD);
+  static const Color greenBorder = Color(0xFFB2E4A1);
+  static const Color greenLight2 = Color(0xFFADD89E);
+  static const Color greenText = Color(0xFF46A624);
+  static const Color greenDark = Color(0xFF2E6B18);
+
+  static const Color yellowActive = Color(0xFFFFEECC);
+  static const Color yellowBorder = Color(0xFFFFB74D);
+  static const Color yellowIcon = Color(0xFFC06000);
+
+  static const Color blue = Color(0xFF40A5DD);
+
+  static const Color grey1 = Color(0xffF5F5F5);
+  static const Color grey2 = Color(0xFFEBEDEE);
+  static const Color grey3 = Color(0xffC8C8C8);
+  static const Color grey4 = Color(0xFFD6D6D6);
+  static const Color grey5 = Color(0xff9B9B9B);
+  static const Color grey6 = Color(0xffAFAFAF);
+  static const Color darkGrey = Color(0xFF333333);
+
+  static const Color brown = Color(0xFFC06000);
+
+  static const Color darkMenuInput = Color(0xFF2C2C2C);
+  static const Color darkBackground = Color(0xFF121212);
+  static const Color darkSurface = Color(0xFF1E1E1E);
+  static const Color black = Color(0xFF000000);
+
+  static const Color darkOnSurface = Color(0xFFE0E0E0);
+  static const Color darkOnBackground = Color(0xFFE0E0E0);
+  static const Color darkText = Color(0xFFE0E0E0);
+  static const Color white = Color(0xFFFFFFFF);
 }
--- lib/core/style/text_style.dart
+++ lib/core/style/text_style.dart
@@ -1,42 +1,167 @@
 import 'package:flutter/material.dart';
 
-import 'app_color.dart';
+import 'build_context.dart';
 
-abstract class Style {
-  static TextStyle get textStyle {
-    return const TextStyle(fontFamily: 'RobotoSlab');
+class AppTextStyles {
+  final BuildContext _context;
+  AppTextStyles(this._context);
+
+  TextStyle get defaultText => TextStyleDixi.defaultText(_context);
+  TextStyle get defaultBoldText => TextStyleDixi.defaultBoldText(_context);
+  TextStyle get greyText => TextStyleDixi.greyText(_context);
+  TextStyle get greyBoldText => TextStyleDixi.greyBoldText(_context);
+  TextStyle get primaryText => TextStyleDixi.primaryText(_context);
+  TextStyle get primaryBoldText => TextStyleDixi.primaryBoldText(_context);
+  TextStyle get lightText => TextStyleDixi.lightText(_context);
+  TextStyle get lightBoldText => TextStyleDixi.lightBoldText(_context);
+  TextStyle get blackText => TextStyleDixi.blackText(_context);
+  TextStyle get blackBoldText => TextStyleDixi.blackBoldText(_context);
+  TextStyle get errorText => TextStyleDixi.errorText(_context);
+  TextStyle get successText => TextStyleDixi.successText(_context);
+
+  TextStyle get defaultTextLineThrough => TextStyleDixi.defaultTextLineThrough(_context);
+  TextStyle get defaultTextUnderline => TextStyleDixi.defaultTextUnderline(_context);
+}
+
+class TextStyleDixi {
+  static const String _fontFamily = 'RobotoSlab';
+  static const TextStyle _baseTextStyle = TextStyle(fontFamily: _fontFamily);
+
+  static TextStyle defaultText(BuildContext context) {
+    return _baseTextStyle.copyWith(
+      color: ThemeColor(context).onSurfaceColor,
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+    );
   }
 
-  static TextStyle defaultStyle = textStyle.copyWith(
-    color: AppColor.kBlack,
-    fontSize: 16,
-    fontWeight: FontWeight.w400,
-  );
-  static TextStyle defaultLightStyle = textStyle.copyWith(
-    color: AppColor.kBackground,
-    fontSize: 16,
-    fontWeight: FontWeight.w400,
-  );
+  static TextStyle defaultBoldText(BuildContext context) {
+    return _baseTextStyle.copyWith(
+      color: ThemeColor(context).onSurfaceColor,
+      fontSize: 14,
+      fontWeight: FontWeight.w700,
+    );
+  }
 
-  static TextStyle primaryStyle = textStyle.copyWith(
-    color: AppColor.kPrimary,
-    fontSize: 16,
-    fontWeight: FontWeight.w500,
-    decorationColor: AppColor.kPrimary,
-  );
+  static TextStyle greyText(BuildContext context) {
+    return _baseTextStyle.copyWith(
+      color: ThemeColor(context).outlineColor,
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+    );
+  }
 
-  static TextStyle darkStyle =
-      textStyle.copyWith(fontSize: 18, fontWeight: FontWeight.w500, color: AppColor.kBlackText);
+  static TextStyle greyBoldText(BuildContext context) {
+    return _baseTextStyle.copyWith(
+      color: ThemeColor(context).outlineColor,
+      fontSize: 14,
+      fontWeight: FontWeight.w700,
+    );
+  }
 
-  static TextStyle whiteStyle = textStyle.copyWith(
-    color: AppColor.kWhite,
-    fontSize: 16,
-    fontWeight: FontWeight.w400,
-  );
+  static TextStyle primaryText(BuildContext context) {
+    return _baseTextStyle.copyWith(
+      color: ThemeColor(context).primaryColor,
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+    );
+  }
 
-   static TextStyle greyLightStyle = textStyle.copyWith(
-    color: AppColor.kgrey2,
-    fontSize: 16,
-    fontWeight: FontWeight.w400,
-  );
+  static TextStyle primaryBoldText(BuildContext context) {
+    return _baseTextStyle.copyWith(
+      color: ThemeColor(context).primaryColor,
+      fontSize: 14,
+      fontWeight: FontWeight.w700,
+    );
+  }
+
+  static TextStyle secondaryText(BuildContext context) {
+    return _baseTextStyle.copyWith(
+      color: ThemeColor(context).secondaryColor,
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle lightText(BuildContext context) {
+    return _baseTextStyle.copyWith(
+      color: ThemeColor(context).onPrimaryColor,
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle lightBoldText(BuildContext context) {
+    return _baseTextStyle.copyWith(
+      color: ThemeColor(context).onPrimaryColor,
+      fontSize: 14,
+      fontWeight: FontWeight.w700,
+    );
+  }
+
+  static TextStyle blackText(BuildContext context) {
+    return _baseTextStyle.copyWith(
+      color: ThemeColor(context).shadowColor,
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle blackBoldText(BuildContext context) {
+    return _baseTextStyle.copyWith(
+      color: ThemeColor(context).onBackgroundColor,
+      fontSize: 14,
+      fontWeight: FontWeight.w700,
+    );
+  }
+
+  static TextStyle errorText(BuildContext context) {
+    return _baseTextStyle.copyWith(
+      color: ThemeColor(context).errorColor,
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle successText(BuildContext context) {
+    return _baseTextStyle.copyWith(
+      color: ThemeColor(context).successColor,
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle successBoldText(BuildContext context) {
+    return _baseTextStyle.copyWith(
+      color: ThemeColor(context).successColor,
+      fontSize: 12,
+      fontWeight: FontWeight.w700,
+    );
+  }
+
+  static TextStyle warningText(BuildContext context) {
+    return _baseTextStyle.copyWith(
+      color: ThemeColor(context).inversePrimary,
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle defaultTextLineThrough(BuildContext context) {
+    return _baseTextStyle.copyWith(
+      color: ThemeColor(context).onSurfaceColor,
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
+
+  static TextStyle defaultTextUnderline(BuildContext context) {
+    return _baseTextStyle.copyWith(
+      color: ThemeColor(context).onSurfaceColor,
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      decoration: TextDecoration.underline,
+    );
+  }
 }
