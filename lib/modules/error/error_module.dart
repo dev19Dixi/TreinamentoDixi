3c7978257238180cd259ae6cac8cf937e32373ba
RenanVolpe <desenvolvimento19@dixiponto.com.br>
2025-06-13 11:45:50 -0300

    feat: create error module

---
+++ lib/modules/error/error_module.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ErrorModule extends StatelessWidget {
+  const ErrorModule({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("404 - Rota não defina"));
+  }
+}
