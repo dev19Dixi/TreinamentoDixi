bbb2c1e6b41f33c7143e21d8d5588edc8be6b7c6
RenanVolpe <desenvolvimento19@dixiponto.com.br>
2025-06-13 11:46:07 -0300

    feat: add icon from string

--- lib/core/utils/controllers/icon_from_string.dart
+++ lib/core/utils/controllers/icon_from_string.dart
@@ -7,5 +7,10 @@ final Map<String, IconData> iconFromString = {
   'person': Icons.person,
   'add': Icons.add,
   'delete': Icons.delete,
-  // Add more as needed
+  'library_books': Icons.library_books_outlined,
+  'account_tree': Icons.account_tree_outlined,
+  'multiple_stop': Icons.multiple_stop_outlined,
+  'add_chart': Icons.add_chart,
+  'help': Icons.help_outline,
+  'exit_to_app': Icons.exit_to_app
 };
