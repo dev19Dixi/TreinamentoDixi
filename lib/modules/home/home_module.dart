0d6690be4781b1171c17436d27ead786f2b75357
RenanVolpe <desenvolvimento19@dixiponto.com.br>
2025-06-16 09:50:19 -0300

    feat: aplicanod ppbar em modulo home

--- lib/modules/home/home_module.dart
+++ lib/modules/home/home_module.dart
@@ -1,28 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:treinamento_mvvm/modules/home/controllers/navigation_home_controller.dart';
 
-class HomeModule extends StatelessWidget {
+import '../app_bar/controllers/app_bar_custom_mixin.dart';
+import '../app_bar/models/app_bar_model.dart';
+import '../app_bar/models/i_home_module.dart';
+import 'controllers/navigation_home_controller.dart';
+
+class HomeModule extends StatefulWidget implements IHomeModule {
   final NavigationHomeController? controller;
 
   const HomeModule({super.key, this.controller});
+
+  @override
+  State<HomeModule> createState() => _HomeModuleState();
+
+  @override
+  AppBarModel get appBarModel => AppBarModel(title: 'Home', listActions: [const Text("Sem ações por aqui")]);
+}
+
+class _HomeModuleState extends State<HomeModule> with AppBarCustomMixin {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => controller ?? NavigationHomeController()),
+        ChangeNotifierProvider(create: (_) => widget.controller ?? NavigationHomeController()),
       ],
-      child: Scaffold(
-        body: Consumer<NavigationHomeController>(builder: (context, provider, child) {
-          return Navigator(
-            key: provider.homeNavigatorKey,
-            initialRoute: '/',
-            onGenerateRoute: (RouteSettings settings) {
-              return provider.provideRoutes(provider.routeName);
-            },
-          );
-        }),
-      ),
+      child: Consumer<NavigationHomeController>(builder: (context, provider, child) {
+        return Navigator(
+          key: provider.homeNavigatorKey,
+          initialRoute: '/',
+          onGenerateRoute: (RouteSettings settings) {
+            return provider.provideRoutes(provider.routeName);
+          },
+        );
+      }),
     );
   }
 }
